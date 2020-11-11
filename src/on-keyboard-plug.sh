aabfc7743e1f34e3c77f379e1aad0c5c7a5639fb
Paul Sutherland <paul@homemade-logic.com>
2020-11-11 17:15:50 -0500

    My personal configuration/setup

--- src/on-keyboard-plug.sh
+++ src/on-keyboard-plug.sh
@@ -3,7 +3,7 @@
 lockfile="/tmp/monitor-input"
 if [ "plugged in" != "$(< $lockfile)" ]; then
     echo "plugged in" > $lockfile
-    ddcutil setvcp --sn=[SERIAL] [FEATURE] x[VALUE]
-    ddcutil setvcp --sn=[SERIAL] [FEATURE] x[VALUE]
-    ddcutil setvcp --sn=[SERIAL] [FEATURE] x[VALUE]
+    ddcutil setvcp --sn=CFV9N6B80KEL 60 x12
+    ddcutil setvcp --sn=CFV9N7BO103L 60 x12
+    ddcutil setvcp --sn=CFV9N6B8058L 60 x12
 fi
--- src/on-keyboard-unplug.sh
+++ src/on-keyboard-unplug.sh
@@ -3,7 +3,7 @@
 lockfile="/tmp/monitor-input"
 if [ "unplugged" != "$(< $lockfile)" ]; then
     echo "unplugged" > $lockfile
-    ddcutil setvcp --sn=[SERIAL] [FEATURE] x[VALUE]
-    ddcutil setvcp --sn=[SERIAL] [FEATURE] x[VALUE]
-    ddcutil setvcp --sn=[SERIAL] [FEATURE] x[VALUE]
+    ddcutil setvcp --sn=CFV9N6B80KEL 60 x0f
+    ddcutil setvcp --sn=CFV9N7BO103L 60 x0f
+    ddcutil setvcp --sn=CFV9N6B8058L 60 x10
 fi
